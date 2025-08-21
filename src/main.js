a685b0fff9220019ad58a1882e69cc26a139bd2a
sarang jain <sarang.jain67@gmail.com>
2025-08-21 10:52:52 +0530

    Day 2 ------- global scope local scope windows object and behaviour let , const , var (all the diffrences , and when we should use) decision making = (memory allocation) diffrence of script nature Truthy falsy check via Booleean() function and !! operatior

--- src/main.js
+++ src/main.js
@@ -3,3 +3,32 @@ import "./style.css";
 const app = document.getElementById("app");
 
 app.innerHTML = `<h1>Modern JavaScript: Basic</h1>`;
+
+// -------
+
+//decision making  conditional logic
+
+console.log(1 == "1"); // value comparision
+console.log(1 === "1"); // strict checking  // value and type comparision
+
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(!!0); // Boolean(0) or !!0
+
+// truthy
+console.log(!!true);
+console.log(Boolean(true));
+console.log(!![]);
+console.log(!!{});
+console.log(!!"0");
+console.log(!!-42);
+console.log(!!42);
+console.log(!!3.14);
+
+// falsy
+console.log(!!false);
+console.log(!!"");
+console.log(!!0);
+console.log(!!undefined);
+console.log(!!null);
+console.log(!!NaN);
