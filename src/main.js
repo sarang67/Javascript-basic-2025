06abbde8003a1d51fe2fe64e74490ae25cd1f4d0
sarang jain <sarang.jain67@gmail.com>
2025-08-28 10:03:19 +0530

    // string properties and indexes // immutabilty in string , pass by value

--- src/main.js
+++ src/main.js
@@ -3,3 +3,58 @@ import "./style.css";
 const app = document.getElementById("app");
 
 app.innerHTML = `<h1>Modern JavaScript: Basic</h1>`;
+
+/*
+// String properties and indexes
+console.log("pratiksha");
+
+console.log(new String("Avinash"));
+let anotherName = new String("Avinash");
+console.log(anotherName[0]);
+
+let name = "pratiksha";
+console.log(name);
+console.log(name[0]);
+
+let myInfo = {
+  name: "akash",
+  age: 25,
+};
+
+// access property .notation
+console.log(myInfo.name);
+console.log(myInfo.age);
+
+// computed member acces []
+console.log(myInfo["name"]);
+console.log(myInfo["age"]);
+
+console.log("-----");
+console.log(new String("Avinash"));
+let anotherName1 = new String("Avinash");
+console.log(anotherName1["0"]);
+console.log(anotherName1[1]);
+console.log(anotherName1[2]);
+console.log(anotherName1["length"]);
+console.log(anotherName1["0"]);
+console.log(anotherName1[anotherName1.length - 1]);
+*/
+
+/*
+// immutability in the string
+// all premitive are immutable , yhey are pass by value
+
+let myName = "akash";
+let studentName = "Pratiksha";
+
+console.log(myName);
+console.log(studentName);
+
+//myName = studentName; //   myName  = "Pratiksha";
+
+myName += "shrivastava"; // myname = myname + "shrivastava"  // "akash" + "shrivastava"
+studentName += "sharma"; // studentName = studentName + "sharma" // "Pratiksha" + "sharma"
+
+console.log(myName);
+console.log(studentName);
+*/
